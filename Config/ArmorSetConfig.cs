c11af315cea429686dc6d91b39c23d09a5c9cc19
RachelsNotEvil <rachelsnotevil@gmail.com>
2026-08-13 16:49:50 -0500

    Add support for Boreal Wood armor set in config and fixed config input ranges to max 65536

--- Config/ArmorSetConfig.cs
+++ Config/ArmorSetConfig.cs
@@ -9,14 +9,30 @@ namespace PermanentSetBonuses.Config
 
 		[Header("WoodArmor")]
 		[Label("Enable Wood Armor")]
+		[Tooltip("Allow players to gain experience for and master basic wood armor.")]
 		[DefaultValue(true)]
 		[ReloadRequired]
 		public bool EnableWood;
 
 		[Label("Wood EXP Required")]
 		[Tooltip("Amount of experience required to master the Wood Armor set.\nDefault 500.")]
+		[Range(0, 65536)]
 		[DefaultValue(500)]
 		[ReloadRequired]
 		public int WoodEXP;
+
+		[Label("Enable Boreal Wood Armor")]
+		[Tooltip("Allow players to gain experience for and master boreal wood armor.")]
+		[DefaultValue(true)]
+		[ReloadRequired]
+		public bool EnableBorealWood;
+
+		[Label("Boreal Wood EXP Required")]
+		[Tooltip("Amount of experince required to master the Boreal Wood Armor set.\nDefault 550.")]
+		[Range(0, 65536)]
+		[DefaultValue(550)]
+		[ReloadRequired]
+		public int BorealEXP;
+
 	}//end class ArmorSetConfig
 }//end namespace PermanentSetBonuses.Config
