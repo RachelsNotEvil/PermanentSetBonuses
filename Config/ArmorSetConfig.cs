2d4c8793c3c5ecbe095d1c4353087280e95e5a1d
RachelsNotEvil <rachelsnotevil@gmail.com>
2026-08-14 09:34:32 -0500

    Add configuration for Palm Wood and Rich Mahogany armor sets.

--- Config/ArmorSetConfig.cs
+++ Config/ArmorSetConfig.cs
@@ -16,7 +16,7 @@ namespace PermanentSetBonuses.Config
 
 		[Label("Wood EXP Required")]
 		[Tooltip("Amount of experience required to master the Wood Armor set.\nDefault 500.")]
-		[Range(0, 65536)]
+		[Range(0, 65535)]
 		[DefaultValue(500)]
 		[ReloadRequired]
 		public int WoodEXP;
@@ -29,10 +29,35 @@ namespace PermanentSetBonuses.Config
 
 		[Label("Boreal Wood EXP Required")]
 		[Tooltip("Amount of experince required to master the Boreal Wood Armor set.\nDefault 550.")]
-		[Range(0, 65536)]
+		[Range(0, 65535)]
 		[DefaultValue(550)]
 		[ReloadRequired]
 		public int BorealEXP;
 
+		[Label("Enable Palm Wood Armor")]
+		[Tooltip("Allow players to gain experience for and master palm wood armor.")]
+		[DefaultValue(true)]
+		[ReloadRequired]
+		public bool EnablePalmWood;
+
+		[Label("Palm Wood EXP Required")]
+		[Tooltip("Amount of experience required to master the Palm Wood Armor set.\nDefault 550.")]
+		[Range(0, 65535)]
+		[DefaultValue(550)]
+		[ReloadRequired]
+		public int PalmEXP;
+
+		[Label("Enable Rich Mahogany")]
+		[Tooltip("Allow players to gain experience for and master rich mahogany armor.")]
+		[DefaultValue(true)]
+		[ReloadRequired]
+		public bool EnableMahogany;
+
+		[Label("Rich Mahogany EXP Required")]
+		[Tooltip("Amount of experience required to master the Rich Mahogany Armor set.\nDefault 575.")]
+		[Range(0, 65535)]
+		[DefaultValue(575)]
+		[ReloadRequired]
+		public int MahoganyEXP;
 	}//end class ArmorSetConfig
 }//end namespace PermanentSetBonuses.Config
